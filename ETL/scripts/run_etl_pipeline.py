cf2e25e2ce761864f945d10ca06b12278978182b
josuengwala27 <josue.ngwala@icloud.com>
2025-06-26 19:07:54 +0200

    Maj 2 comments

--- ETL/scripts/run_etl_pipeline.py
+++ ETL/scripts/run_etl_pipeline.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Script principal orchestrant le pipeline ETL complet
-Auteur: MSPR Project
-Date: 2024
-Description: Execute automatiquement toutes les etapes ETL
-"""
 
 import os
 import sys
